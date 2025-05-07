403247c330c29ec696967d61ab5c3f65440fe1a8
luigiay1123 <wemixmir801@gmail.com>
2025-05-07 14:13:15 +0800

    add project 06 and 07

---
+++ projects/project07/script.js
@@ -0,0 +1,8 @@
+// AOS.init
+
+AOS.init({
+  delay: 200, // values from 0 to 3000, with steps of 50ms
+  duration: 1500, // values from 0 to 3000, with steps of 50ms
+  once: false,
+  mirror: false,
+});
